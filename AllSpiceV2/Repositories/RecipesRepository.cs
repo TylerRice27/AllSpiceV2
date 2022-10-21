ab4c51bb73cc86667b9ba9e58e5f95e13f73d2ea
Jeffrey Watson <watsonjeffrey44@gmail.com>
2022-10-21 17:23:43 -0600

    more

--- AllSpiceV2/Repositories/RecipesRepository.cs
+++ AllSpiceV2/Repositories/RecipesRepository.cs
@@ -1,30 +1,29 @@
 using System.Data;
 using AllSpiceV2.Models;
-using Dapper;
 
 namespace AllSpiceV2.Repositories
 {
-    public class RecipesRepository
+  public class RecipesRepository
+  {
+    private readonly IDbConnection _db;
+
+    public RecipesRepository(IDbConnection db)
     {
-        private readonly IDbConnection _db;
-
-        public RecipesRepository(IDbConnection db)
-        {
-            _db = db;
-        }
-
-        internal Recipe Create(Recipe newRecipe)
-        {
-
-            string sql = @"
-           INSERT INTO tjrecipes
-           (title, img, instructions, category, creatorId)
-           VALUES
-           (@Title, @Img, @Instructions, @Category, @CreatorId)";
-            _db.Execute(sql, newRecipe);
-            return newRecipe;
-
-        }
+      _db = db;
     }
+
+    internal Recipe Create(Recipe newRecipe)
+    {
+
+      string sql = @"
+            INSERT INTO tjrecipes
+            (title, img, instructions, category, creatorId)
+            VALUES
+            (@Title, @Img, @Instructions, @Category, @CreatorId)";
+      _db.Execute(sql, newRecipe);
+      return newRecipe;
+
+    }
+  }
 
 }
