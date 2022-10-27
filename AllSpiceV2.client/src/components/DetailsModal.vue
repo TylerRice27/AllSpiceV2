1ce82a67a28bc4043f8d0cf802cc44eefe520b3a
TylerRice27 <Tylerrayrice2@gmail.com>
2022-10-27 16:01:17 -0600

    Detail modal rough draft

--- AllSpiceV2.client/src/components/DetailsModal.vue
+++ AllSpiceV2.client/src/components/DetailsModal.vue
@@ -4,18 +4,23 @@
     <div class="col-md-5">
       <img class="img-fluid custom" :src="activeRecipe.img" alt="" />
     </div>
-    <div class="col-md-3">
+
+    <div class="col-md-3 ms-4 mt-5 mb-5">
       <div class="card h-100 card-color">
         <h3 class="text-center glass4 text-white">Recipe Instructions</h3>
 
-        <p>{{ activeRecipe.instructions }}</p>
+        <p class="ps-3">{{ activeRecipe.instructions }}</p>
+      </div>
+    </div>
+
+    <div class="col-md-3 ms-4 mt-5 mb-5">
+      <div class="card h-100 card-color">
+        <h3 class="text-center glass4 text-white">Recipe Instructions</h3>
+
+        <p class="ps-3">{{ activeRecipe.ingredients }}</p>
       </div>
     </div>
   </div>
-
-  <div class="col-md-3"></div>
-
-  <!-- </div> -->
 </template>
 
 
