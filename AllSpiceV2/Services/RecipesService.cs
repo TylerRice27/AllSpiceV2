59dce4f69806b400de8b9d401ce49da1ae9d1799
Jeffrey Watson <watsonjeffrey44@gmail.com>
2022-11-02 16:26:29 -0600

    random

--- AllSpiceV2/Services/RecipesService.cs
+++ AllSpiceV2/Services/RecipesService.cs
@@ -5,63 +5,63 @@ using AllSpiceV2.Repositories;
 
 namespace AllSpiceV2.Services
 {
-    public class RecipesService
+  public class RecipesService
+  {
+    private readonly RecipesRepository _repo;
+
+    public RecipesService(RecipesRepository repo)
     {
-        private readonly RecipesRepository _repo;
-
-        public RecipesService(RecipesRepository repo)
-        {
-            _repo = repo;
-        }
-
-        internal Recipe Create(Recipe newRecipe)
-        {
-            return _repo.Create(newRecipe);
-        }
-
-        internal List<Recipe> GetAll()
-        {
-            return _repo.GetAll();
-        }
-
-        internal Recipe GetById(int id)
-        {
-
-            Recipe recipe = _repo.GetById(id);
-            if (recipe == null)
-            {
-                throw new Exception("No Recipe by That Id");
-            }
-            return recipe;
-        }
-
-        internal Recipe Edit(Recipe update, string userId)
-        {
-            Recipe original = GetById(update.Id);
-            if (original.CreatorId != userId)
-            {
-                throw new Exception("You are not the owner of this recipe");
-            }
-            original.Title = update.Title ?? original.Title;
-            original.Instructions = update.Instructions ?? original.Instructions;
-            original.Category = update.Category ?? original.Category;
-            original.Img = update.Img ?? original.Img;
-
-            return _repo.Edit(original);
-
-        }
-
-        internal string Delete(int id, string userId)
-        {
-
-            Recipe original = GetById(id);
-            if (original.CreatorId != userId)
-            {
-                throw new Exception("You can not delete this recipe");
-
-            }
-            _repo.Delete(id);
-            return $"You deleted {original.Title}";
-        }
+      _repo = repo;
     }
+
+    internal Recipe Create(Recipe newRecipe)
+    {
+      return _repo.Create(newRecipe);
+    }
+
+    internal List<Recipe> GetAll()
+    {
+      return _repo.GetAll();
+    }
+
+    internal Recipe GetById(int id)
+    {
+
+      Recipe recipe = _repo.GetById(id);
+      if (recipe == null)
+      {
+        throw new Exception("No Recipe by That Id");
+      }
+      return recipe;
+    }
+
+    internal Recipe Edit(Recipe update, string userId)
+    {
+      Recipe original = GetById(update.Id);
+      if (original.CreatorId != userId)
+      {
+        throw new Exception("You are not the owner of this recipe");
+      }
+      original.Title = update.Title ?? original.Title;
+      original.Instructions = update.Instructions ?? original.Instructions;
+      original.Category = update.Category ?? original.Category;
+      original.Img = update.Img ?? original.Img;
+
+      return _repo.Edit(original);
+
+    }
+
+    internal string Delete(int id, string userId)
+    {
+
+      Recipe original = GetById(id);
+      if (original.CreatorId != userId)
+      {
+        throw new Exception("You can not delete this recipe");
+
+      }
+      _repo.Delete(id);
+      return $"You deleted {original.Title}";
+    }
+  }
 }
