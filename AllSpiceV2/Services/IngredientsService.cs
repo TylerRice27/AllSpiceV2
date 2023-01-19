60544411004f8d536d2a610e4aae4f760c99a52a
TylerRice27 <Tylerrayrice2@gmail.com>
2023-01-19 12:06:06 -0700

    V-if on the form to create ingredient

--- AllSpiceV2/Services/IngredientsService.cs
+++ AllSpiceV2/Services/IngredientsService.cs
@@ -5,61 +5,61 @@ using AllSpiceV2.Repositories;
 
 namespace AllSpiceV2.Services
 {
-  public class IngredientsService
-  {
-    private readonly IngredientsRepository _repo;
-    private readonly RecipesService _rs;
-
-    public IngredientsService(IngredientsRepository repo, RecipesService rs)
+    public class IngredientsService
     {
-      _repo = repo;
-      _rs = rs;
+        private readonly IngredientsRepository _repo;
+        private readonly RecipesService _rs;
+
+        public IngredientsService(IngredientsRepository repo, RecipesService rs)
+        {
+            _repo = repo;
+            _rs = rs;
+        }
+
+        internal Ingredient Create(Ingredient newIngredient)
+        {
+            return _repo.Create(newIngredient);
+        }
+
+        internal Ingredient GetById(int id)
+        {
+            Ingredient found = _repo.GetById(id);
+            if (found == null)
+            {
+                throw new Exception("Invalid Id");
+            }
+            return found;
+        }
+
+        public List<Ingredient> GetIngredientsByRecipe(int recipeId)
+        {
+            return _repo.GetIngredientsByRecipe(recipeId);
+        }
+
+        internal Ingredient Delete(int id, string userId)
+        {
+            Ingredient original = this.GetById(id);
+            // Not required to pass postman test but this is business logic for security
+            Recipe found = _rs.GetById(original.RecipeId);
+            if (found.CreatorId != userId)
+            {
+                throw new Exception("This is not yours.");
+            }
+            _repo.Delete(original);
+            return original;
+        }
+
+        internal Ingredient Edit(Ingredient update, string userId)
+        {
+            Ingredient original = GetById(update.Id);
+            if (original.Creator.Id != userId)
+            {
+                throw new Exception("You are not the owner of this recipe");
+            }
+            original.Name = update.Name ?? original.Name;
+            original.Quantity = update.Quantity ?? original.Quantity;
+
+            return _repo.Edit(original);
+        }
     }
-
-    internal Ingredient Create(Ingredient newIngredient)
-    {
-      return _repo.Create(newIngredient);
-    }
-
-    internal Ingredient GetById(int id)
-    {
-      Ingredient found = _repo.GetById(id);
-      if (found == null)
-      {
-        throw new Exception("Invalid Id");
-      }
-      return found;
-    }
-
-    public List<Ingredient> GetIngredientsByRecipe(int recipeId)
-    {
-      return _repo.GetIngredientsByRecipe(recipeId);
-    }
-
-    internal Ingredient Delete(int id, string userId)
-    {
-      Ingredient original = this.GetById(id);
-      // Not required to pass postman test but this is business logic for security
-      Recipe found = _rs.GetById(original.RecipeId);
-      if (found.CreatorId != userId)
-      {
-        throw new Exception("This is not yours.");
-      }
-      _repo.Delete(original);
-      return original;
-    }
-
-    // internal Ingredient Edit(Ingredient update, string userId)
-    // {
-    //   Ingredient original = GetById(update.Id);
-    //   if (original.Creator.Id != userId)
-    //   {
-    //     throw new Exception("You are not the owner of this recipe");
-    //   }
-    //   original.Name = update.Name ?? original.Name;
-    //   original.Quantity = update.Quantity ?? original.Quantity;
-
-    //   return _repo.Edit(original);
-    // }
-  }
 }
