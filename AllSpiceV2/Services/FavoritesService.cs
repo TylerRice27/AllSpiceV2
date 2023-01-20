9a428e647141045f1f820feafc6e3d85383952b1
TylerRice27 <Tylerrayrice2@gmail.com>
2023-01-19 17:12:16 -0700

    Left a note for create Favorite

--- AllSpiceV2/Services/FavoritesService.cs
+++ AllSpiceV2/Services/FavoritesService.cs
@@ -5,54 +5,63 @@ using AllSpiceV2.Repositories;
 
 namespace AllSpiceV2.Services
 {
-  public class FavoritesService
-  {
-
-    private readonly FavoritesRepository _repo;
-
-    private readonly RecipesService _rs;
-
-    public FavoritesService(FavoritesRepository repo, RecipesService rs)
+    public class FavoritesService
     {
-      _repo = repo;
-      _rs = rs;
+
+        private readonly FavoritesRepository _repo;
+
+        private readonly RecipesService _rs;
+
+        public FavoritesService(FavoritesRepository repo, RecipesService rs)
+        {
+            _repo = repo;
+            _rs = rs;
+        }
+
+        internal Favorite Create(Favorite newFavorite)
+        {
+            Recipe recipe = _rs.GetById(newFavorite.RecipeId);
+
+
+            recipe.Id = newFavorite.RecipeId;
+            // NOTE Come Back Here Later We are going to have to write an unquie column/index in sql
+            // NOTE OR we can write a new route to our favorites table and do a getById  and if it the one we are creating matches
+            // the new recipe id and the creator id then throw an error 
+
+            // if ()
+            // {
+            //     throw new Exception("You have already favorited this recipe");
+            // }
+            return _repo.Create(newFavorite);
+        }
+
+        internal List<FavoritedRecipe> GetAccountFavorites(string userId)
+        {
+            return _repo.GetAccountFavorites(userId);
+        }
+
+        internal void Delete(int id, string userId)
+        {
+            Favorite original = this.GetById(id);
+            if (original.AccountId != userId)
+            {
+                throw new Exception("You can not remove this favorite");
+
+            }
+            _repo.Delete(id);
+            // return $" {original.Creator.Name} unfavorited this recipe";
+
+        }
+
+        internal Favorite GetById(int id)
+        {
+            Favorite favorite = _repo.GetById(id);
+            if (favorite == null)
+            {
+
+                throw new Exception("No Favorite by that Id");
+            }
+            return favorite;
+        }
     }
-
-    internal Favorite Create(Favorite newFavorite)
-    {
-      Recipe recipe = _rs.GetById(newFavorite.RecipeId);
-
-      recipe.Id = newFavorite.RecipeId;
-      return _repo.Create(newFavorite);
-    }
-
-    internal List<FavoritedRecipe> GetAccountFavorites(string userId)
-    {
-      return _repo.GetAccountFavorites(userId);
-    }
-
-    internal void Delete(int id, string userId)
-    {
-      Favorite original = this.GetById(id);
-      if (original.AccountId != userId)
-      {
-        throw new Exception("You can not remove this favorite");
-
-      }
-      _repo.Delete(id);
-      // return $" {original.Creator.Name} unfavorited this recipe";
-
-    }
-
-    internal Favorite GetById(int id)
-    {
-      Favorite favorite = _repo.GetById(id);
-      if (favorite == null)
-      {
-
-        throw new Exception("No Favorite by that Id");
-      }
-      return favorite;
-    }
-  }
 }
