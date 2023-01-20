5ffd8a729391d4a30f6931a3ea6f2983fba97941
TylerRice27 <Tylerrayrice2@gmail.com>
2023-01-20 11:32:54 -0700

    Got my onblur working and editMode

--- AllSpiceV2.client/src/components/Ingredient.vue
+++ AllSpiceV2.client/src/components/Ingredient.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="d-flex justify-content-around height">
-    <p>
-      {{ ingredient.quantity }}
-      {{ ingredient.name }}
-    </p>
+    <li>
+      <input
+        @blur="editIngredient"
+        v-if="editMode"
+        v-model="ingredient.quantity"
+      />
+      <input @blur="editIngredient" v-if="editMode" v-model="ingredient.name" />
+      <div v-else>
+        <label class="px-1" v-if="!editMode">{{ ingredient.quantity }}</label>
+        <label class="px-1" v-if="!editMode">{{ ingredient.name }}</label>
+      </div>
+    </li>
     <div v-if="activeRecipe.creatorId == account.id">
       <i
         class="mdi mdi-pencil-outline fs-5 p-1 selectable"
-        @click="editIngredient"
+        @click="editMode = true"
       ></i>
       <i
         class="mdi mdi-delete-forever-outline fs-5 text-danger selectable"
@@ -19,7 +27,7 @@
 
 
 <script>
-import { computed } from '@vue/runtime-core'
+import { computed, ref } from '@vue/runtime-core'
 import { AppState } from '../AppState.js';
 import { ingredientService } from '../services/IngredientService.js';
 import { logger } from '../utils/Logger.js';
@@ -29,7 +37,10 @@ import Pop from '../utils/Pop.js';
 export default {
   props: { ingredient: { type: Object, required: true } },
   setup(props) {
+    const editMode = ref(false)
+
     return {
+      editMode,
       ingredients: computed(() => AppState.ingredients),
       activeRecipe: computed(() => AppState.activeRecipe),
       account: computed(() => AppState.account),
@@ -47,6 +58,7 @@ export default {
       async editIngredient() {
         try {
           await ingredientService.editIngredient(props.ingredient.id,)
+          editMode.value = false
         } catch (error) {
           logger.error(error)
           Pop.toast(error.message, 'error')
