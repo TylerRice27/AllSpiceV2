37b2775aac23f7c5a0ecb1616e51fd3aa9f61be5
TylerRice27 <Tylerrayrice2@gmail.com>
2023-01-25 15:01:58 -0700

    new glass six new and improved

--- AllSpiceV2.client/src/components/Login.vue
+++ AllSpiceV2.client/src/components/Login.vue
@@ -1,65 +1,65 @@
 <template>
-	<span class="navbar-text">
-		<button
-			class="
-				btn
-				selectable
-				text-white
-				glass
-				lighten-30
-				text-uppercase
-				my-2 my-lg-0
-			"
-			@click="login"
-			v-if="!user.isAuthenticated"
-		>
-			Login
-		</button>
-		<div v-else>
-			<div class="dropdown dropstart my-2 my-lg-0">
-				<div
-					type="button"
-					class="border-0 selectable no-select"
-					data-bs-toggle="dropdown"
-					aria-expanded="false"
-				>
-					<div v-if="account.picture || user.picture">
-						<img
-							:src="account.picture || user.picture"
-							alt="account photo"
-							height="40"
-							class="rounded"
-						/>
-					</div>
-				</div>
-				<div
-					class="dropdown-menu dropdown-menu-lg-left p-0"
-					aria-labelledby="authDropdown"
-				>
-					<div class="list-group">
-						<router-link :to="{ name: 'Account' }">
-							<div class="list-group-item dropdown-item list-group-item-action">
-								Manage Account
-							</div>
-						</router-link>
-						<div
-							class="
-								list-group-item
-								dropdown-item
-								list-group-item-action
-								text-danger
-								selectable
-							"
-							@click="logout"
-						>
-							<i class="mdi mdi-logout"></i>
-							logout
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</span>
+  <span class="navbar-text">
+    <button
+      class="
+        btn
+        selectable
+        text-white
+        glass
+        lighten-30
+        text-uppercase
+        my-2 my-lg-0
+      "
+      @click="login"
+      v-if="!user.isAuthenticated"
+    >
+      Login
+    </button>
+    <div v-else>
+      <div class="dropdown dropstart my-2 my-lg-0">
+        <div
+          type="button"
+          class="border-0 selectable no-select"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <div v-if="account.picture || user.picture">
+            <img
+              :src="account.picture || user.picture"
+              alt="account photo"
+              height="40"
+              class="rounded glass6"
+            />
+          </div>
+        </div>
+        <div
+          class="dropdown-menu dropdown-menu-lg-left p-0"
+          aria-labelledby="authDropdown"
+        >
+          <div class="list-group">
+            <router-link :to="{ name: 'Account' }">
+              <div class="list-group-item dropdown-item list-group-item-action">
+                Manage Account
+              </div>
+            </router-link>
+            <div
+              class="
+                list-group-item
+                dropdown-item
+                list-group-item-action
+                text-danger
+                selectable
+              "
+              @click="logout"
+            >
+              <i class="mdi mdi-logout"></i>
+              logout
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </span>
 </template>
 
 <script>
@@ -67,18 +67,18 @@ import { computed } from 'vue'
 import { AppState } from '../AppState'
 import { AuthService } from '../services/AuthService'
 export default {
-	setup() {
-		return {
-			user: computed(() => AppState.user),
-			account: computed(() => AppState.account),
-			async login() {
-				AuthService.loginWithPopup()
-			},
-			async logout() {
-				AuthService.logout({ returnTo: window.location.origin })
-			}
-		}
-	}
+  setup() {
+    return {
+      user: computed(() => AppState.user),
+      account: computed(() => AppState.account),
+      async login() {
+        AuthService.loginWithPopup()
+      },
+      async logout() {
+        AuthService.logout({ returnTo: window.location.origin })
+      }
+    }
+  }
 }
 </script>
 
