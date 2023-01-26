252aa4308b01c973346acfcacdeb55fff1ede62f
TylerRice27 <Tylerrayrice2@gmail.com>
2023-01-25 17:58:16 -0700

    Push for Render

--- AllSpiceV2.client/vite.config.js
+++ AllSpiceV2.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../AllSpiceV2/wwwroot',
     sourcemap: false
   },
   server: {
